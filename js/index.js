da61b5a32311ef5a0da1280a0bdfe2d85994790b
asror797 <asror797shahobiddinov@gmail.com>
2021-11-02 10:00:46 +0300

    deploy

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// Variables
+
+var userName=prompt("Ismingizni kiriting: ");
+var userAge=prompt("Yoshigizni kiriting: ");
+var userDate=prompt("Tug'ilgan oyingizni raqamini kiriting: " );
+var userDateDay=prompt("Tug'ilgan sanangizni kiriting: ");
+var lifeTime=((2021-userAge)*360)-30;
+
+alert(userName+ " " + lifeTime + " " + "kunni boshizdan o'tkazdingiz :)");
